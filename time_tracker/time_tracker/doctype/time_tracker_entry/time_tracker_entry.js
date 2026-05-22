9734a843b9214265b396bbce0ffa5a635a4ee90f
muqeetmughal <muqeetmughal786@gmail.com>
2026-05-22 21:49:01 +0500

    feat: organize doctype field layouts with tabs/sections, make Time Tracker Entry read-only after creation

--- time_tracker/time_tracker/doctype/time_tracker_entry/time_tracker_entry.js
+++ time_tracker/time_tracker/doctype/time_tracker_entry/time_tracker_entry.js
@@ -1,8 +1,10 @@
 // Copyright (c) 2026, Muqeet Mughal and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Time Tracker Entry", {
-// 	refresh(frm) {
-
-// 	},
-// });
+frappe.ui.form.on("Time Tracker Entry", {
+	refresh(frm) {
+		if (!frm.is_new()) {
+			frm.disable_form();
+		}
+	},
+});
